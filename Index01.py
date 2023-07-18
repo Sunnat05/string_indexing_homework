bed5b64538b06133b0562fb6868a5cce613fd09b
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-07-18 23:16:49 +0500

    nth

--- Index01.py
+++ Index01.py
@@ -1,5 +1,4 @@
 def main(s):
-    s="parameter"
     """
     The string variable s is given. return the first character.
     Args:
@@ -8,4 +7,4 @@ def main(s):
         str: answer
     """
     return s[0]
-print(main("parameter")) 
+print(main("mff")) 
