292016eaf585d9022e383ed8caa9b7a880eab458
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-07-19 09:14:36 +0500

    nth

--- Index05.py
+++ Index05.py
@@ -1,4 +1,16 @@
 def main(s):
+    a=0
+    if s[0] in "0123456789":
+        a+=1   
+    if s[1] in "0123456789":
+        a+=1
+    if s[2] in "0123456789":
+        a+=1
+    if s[3] in "0123456789":
+        a+=1
+    if s[4] in "0123456789":
+        a+=1
+
     """
     Given a variable s string of length five. Determine the number of digits involved in this variable.
     Args:
@@ -6,4 +18,5 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    return a
+print(main("2d5f6"))
