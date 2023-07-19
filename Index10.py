2d91f735b94b3247f1df21e05c48e1466b4eb07c
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-07-19 21:15:24 +0500

    nth

--- Index10.py
+++ Index10.py
@@ -1,4 +1,15 @@
 def main(s):
+    a=0
+    if s[0] in "0123456789":
+        a+=int(s(0))
+    if s[1] in "0123456789":
+        a+=int(s[1])
+    if s[2] in "0987654321":
+        a+=int(s[2])
+    if s[3] in "1234567890":
+        a+=int(s[3])
+    if s[4] in "1234567890":
+        a+=int(s[4])
     """
     A string of five numbers is given. Find the sum of its numbers.
     Args:
@@ -6,4 +17,5 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    return a
+print(main("dfw45"))
